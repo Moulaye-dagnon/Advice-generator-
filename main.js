e452655c033832fa16dce3de7fe40a30fee1d9ca
Moulaye-dagnon <moulayedagnon@gmail.com>
2023-11-06 10:26:53 +0100

    Update my function

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 import { fetchUsers} from "./function/api.js"
 
-const link = await fetchUsers( "https://api.adviceslip.com/advice")
 const content = document.querySelector('.container')
 const elementId  =  document.querySelector('.id')
 const elementadvice  =  document.querySelector('.advice')
-elementId.innerHTML = `ADVICE # ${link.slip.id}`
-elementadvice.innerHTML = `" ${link.slip.advice} "`
-document.querySelector('button').addEventListener('click',(e)=>{
-    e.preventDefault()
-    window.location.reload()
+
+async function a(){
+    const link = await fetchUsers( "https://api.adviceslip.com/advice")
+    elementId.innerHTML = `ADVICE # ${link.slip.id}`
+    elementadvice.innerHTML = `" ${link.slip.advice} "`
+
+}
+a()
+document.querySelector('button').addEventListener('click',()=>{
+    a()
 })
 
 
